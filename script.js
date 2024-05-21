78b4fa7c61ddc16c8b4d7e330511f826bd972ed4
JaswanthYenugudhati <jaswanthyenugudhati@gmail.com>
2024-05-21 18:22:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const About1=document.getElementById("About");
+const about2=document.getElementById("about");
+
+About1.setAttribute("href",`#${about2.id}`);
+
+const Service1 = document.getElementById("Services")
+const Service2 = document.getElementById("service")
+
+Service1.setAttribute("href",`#${Service2.id}`);
+
+const Contact1 = document.getElementById("Contact")
+const Contact2 = document.getElementById("contact")
+
+Contact1.setAttribute("href",`#${Contact2.id}`);
